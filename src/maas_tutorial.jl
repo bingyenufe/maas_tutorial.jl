92502a9d337a16294e2fd389e59dbb1a9ffd7a5c
bingyenufe <121657961@163.com>
2025-08-16 18:28:31 +0800

    init

--- src/maas_tutorial.jl
+++ src/maas_tutorial.jl
@@ -1,5 +1,16 @@
+"Simple dummy project module to demonstrate how a project can be organized."
 module maas_tutorial
 
-# Write your package code here.
+export ArithmeticSum
 
+"""
+    ArithmeticSum(a₁,Δ,n)
+ 
+Returns the sum of the arithmetic sequence starting from a₁, 
+with interval Δ and n terms.
+"""
+function ArithmeticSum(a₁,Δ,n) 
+    return (n+1)*(a₁ + (a₁+n*Δ))/2
 end
+
+end # module maas_tutorial
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using maas_tutorial
 using Test
 
-@testset "maas_tutorial.jl" begin
-    # Write your tests here.
+SlowSum(a₁,Δ,n) = sum([a₁+Δ*i for i ∈ 0:n])
+
+@testset "ArithmeticSum" begin
+    @test ArithmeticSum(1,1,14) == SlowSum(1,1,14)
+    @test ArithmeticSum(5,1,10) == SlowSum(5,1,10)
+    @test ArithmeticSum(2,3,14) == SlowSum(2,3,14)
 end
